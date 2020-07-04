4fa0d6801ad6fd6794c07bb80517be9755a0a0df
Suresh-Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-07-04 21:12:51 +0530

    Working sample

--- ProtocolBuffers/Program.cs
+++ ProtocolBuffers/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Google.Protobuf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,21 @@ namespace ProtocolBuffers
     {
         static void Main(string[] args)
         {
+            Person person = new Person { Id = 1, Name = "Suresh" };
+            var binary = Encode(person);
+            var outPerson = Decode(binary);
+        }
+        static byte[] Encode(Person person)
+        {
+            using (var stream = new MemoryStream())
+            {
+                person.WriteTo(stream);
+                return stream.ToArray();
+            }
+        }
+        static Person Decode(byte[] stream)
+        {
+            return Person.Parser.ParseFrom(stream);
         }
     }
 }
