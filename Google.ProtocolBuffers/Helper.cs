1bd0a0e28ab8f1ea8a30edec8cdda3dd44f79efe
Suresh-Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-07-14 13:28:56 +0530

    Moving solution to parent directory

---
+++ Google.ProtocolBuffers/Helper.cs
@@ -0,0 +1,50 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using KellermanSoftware.CompareNetObjects;
+using KellermanSoftware.CompareNetObjects.TypeComparers;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Tynamix.ObjectFiller;
+
+namespace Google.ProtocolBuffers
+{
+    class Helper
+    {
+        public static byte[] Encode<T>(T anObject) where T : IMessage
+        {
+            using (var stream = new MemoryStream())
+            {
+                anObject.WriteTo(stream);
+                return stream.ToArray();
+            }
+        }
+
+        public static K Decode<K>(byte[] stream, MessageParser<K> parser) where K : IMessage<K>
+        {
+            return parser.ParseFrom(stream);
+        }
+
+        public static T FillObject<T>() where T : class
+        {
+            var randomStringGenerator  = new MnemonicString(2);
+
+            var filler = new Filler<T>();
+            filler.Setup().OnType<ByteString>().Use(ByteString.CopyFrom(randomStringGenerator.GetValue(), Encoding.Unicode));
+            filler.Setup().OnType<Timestamp>().Use(Timestamp.FromDateTime(DateTime.UtcNow)); //should be UTC
+            filler.Setup().OnType<Duration>().Use(Duration.FromTimeSpan(new TimeSpan(100)));
+            return filler.Create();
+            
+        }
+
+        public static bool CompareObjects<T>(T o, T b)
+        {
+            CompareLogic compareLogic = new CompareLogic();
+            compareLogic.Config.CustomComparers.Add(new CustomComparer<ByteString, ByteString>((o, b) => o.Equals(b)));
+            var result = compareLogic.Compare(o, b);
+            return result.AreEqual;
+        }
+    }
+}
---
+++ Google.ProtocolBuffers/Program.cs
@@ -0,0 +1,53 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace Google.ProtocolBuffers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = Helper.FillObject<Person>();
+            var binary = Helper.Encode(person);
+            var decode = Helper.Decode(binary, Person.Parser);
+            Assert.IsTrue(Helper.CompareObjects(person, decode));
+
+            Advanced.AdvancedConcepts();
+        }
+    }
+    class Advanced
+    {
+        public static void AdvancedConcepts()
+        {
+            ScalarTypes scalarType = Helper.FillObject<ScalarTypes>();
+            var scalarTypeEncode = Helper.Encode(scalarType);
+            var scalarTypeDecode = Helper.Decode(scalarTypeEncode, ScalarTypes.Parser);
+            Assert.IsTrue(Helper.CompareObjects(scalarType, scalarTypeDecode));
+
+            ComplexTypes complexType = Helper.FillObject<ComplexTypes>();
+            complexType.Maps.Add(1, "Suresh");
+            complexType.Numbers.Add(10);
+            complexType.Details = Any.Pack(Helper.FillObject<Person>());
+            var complexTypeEncode = Helper.Encode(complexType);
+            var complexTypeDecode = Helper.Decode(complexTypeEncode, ComplexTypes.Parser);
+            Assert.IsTrue(Helper.CompareObjects(complexType, complexTypeDecode));
+
+            if (complexTypeDecode.Details.Is(Person.Descriptor))
+                complexTypeDecode.Details.TryUnpack(out Person person);
+            var oneOfCase = complexTypeDecode.OneofCase;    //returns the name of the field that is set 
+
+            NullableTypes nullableType = Helper.FillObject<NullableTypes>();
+            var nullableTypeEncode = Helper.Encode(nullableType);
+            var nullableTypeDecode = Helper.Decode(nullableTypeEncode, NullableTypes.Parser);
+            Assert.IsTrue(Helper.CompareObjects(nullableType, nullableTypeDecode));
+
+            TimeTypes timeType = Helper.FillObject<TimeTypes>();
+            var timeTypeEncode = Helper.Encode(timeType);
+            var timeTypeDecode = Helper.Decode(timeTypeEncode, TimeTypes.Parser);
+            Assert.IsTrue(Helper.CompareObjects(timeType, timeTypeDecode));
+        }
+    }
+}
